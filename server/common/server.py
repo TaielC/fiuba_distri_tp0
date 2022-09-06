ef330e137613a883207ddec59aa07623ab467521
tc <43725160+TaielC@users.noreply.github.com>
2022-09-05 23:24:58 -0300

    Ejercicio 4: Manejo de SIGTERM en cliente y servidor

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,22 @@
+import signal
 import socket
 import logging
+from threading import Event
+
+class TerminationSignal(Exception):
+    pass
+
+
+def _set_sigterm_handler():
+    """
+    Setup SIGTERM signal handler. Raises StopServer exception when SIGTERM
+    signal is received
+    """
+    def signal_handler(_signum, _frame):
+        logging.info("SIGTERM received")
+        raise TerminationSignal()
+
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
 class Server:
@@ -9,7 +26,7 @@ class Server:
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
 
-    def run(self):
+    def run(self, set_sigterm_handler=True):
         """
         Dummy Server loop
 
@@ -17,12 +34,30 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
+        if set_sigterm_handler:
+            _set_sigterm_handler()
+        
+        try:
+            while True:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+        # Handle SIGINT signal
+        except (KeyboardInterrupt, TerminationSignal) as e:
+            logging.info(f"Interrupt received. Stopping server {e}")
+        
+        self._shutdown()
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+    def _stay_alive(self):
+        return not self.shutdown_event.is_set()
+
+    def _shutdown(self):
+        """
+        Shutdown server
+
+        Close server socket and wait for all threads to finish
+        """
+        logging.info("Shutting down server")
+        self._server_socket.close()
 
     def __handle_client_connection(self, client_sock):
         """
